339c85f041cefc63cd27e557c75d9cebb6cc43cb
honcheng <honcheng@gmail.com>
2011-04-09 11:57:30 +0800

    - added plain page

--- config.py
+++ config.py
@@ -18,6 +18,7 @@ theme = 'default'
 page_templates = {
 	'Theme.html': 'Theme',
 	'Simple.html': 'Simple',
+	'Plain.html': 'Plain',
 }
 
 # Defines the URL organization to use for blog postings. Valid substitutions:
